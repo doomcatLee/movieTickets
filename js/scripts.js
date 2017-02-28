da99a64c38096fb622f361e04d00d8fe09ac5fbd
Alex Lee <doomcat9167@gmail.com>
2017-02-28 09:55:01 -0800

    complete add of backend and front and with some basic css. Fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
+// Back-End
+
+function Ticket(name, time, age) {
+    this.name = name;
+    this.time = time;
+    this.age = age;
+}
+
+Ticket.prototype.calculateCost = function(cost) {
+    debugger;
+    if (this.time === ("Afternoon" || "Morning")) {
+        cost -= 10;
+        return cost;
+    } else if (this.age > 50) {
+        cost -= 10;
+        return cost;
+    } else if (this.name === "Shawshank Redemption") {
+        cost -= 10;
+        return cost;
+    } else {
+        return cost;
+    }
+}
 //jQuery BS
 $(function() {
+    $("#newMovie").submit(function() {
+        event.preventDefault();
+        var movie = $("#movie").val();
+        var time = $("#time").val();
+        var age = $("#age").val();
+        // console.log(movie, time, age);
 
-});
+        var customer = new Ticket(movie, time, age);
+        var cost = customer.calculateCost(30);
+
+        $(".movieName").text(movie);
+        $(".movieTime").text(time);
+        $(".cost").text("$" + cost);
+        $("#show-ticket").show();
+
+    }); //submit
+}); //jQuery
